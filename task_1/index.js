b0975c605cced575ff79cdb18def785a6d9ca9a5
Глеб Климчук <glebklimcuk@MacBook-Air-Gleb-2.local>
2024-04-24 13:49:44 +0300

    tasks completed

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,33 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeFields(encoded, translations) {
+    const cosntKeys = ['groupId', 'service', 'formatSize', 'ca']; // поля, которые нужно оставить в изначальном виде
+  
+    let uniqueIds = new Set(); // уникальные id 
+  
+    let decodedList = encoded.map(item => {
+      let decodedItem = {};
+  
+      for (let key in item) {
+        if (key.endsWith('Id') && !cosntKeys.includes(key)) {
+          if (item[key] !== null && translations.hasOwnProperty(item[key])) {
+            uniqueIds.add(item[key]); // добавляем уникальные id
+          }
+          decodedItem[key] = translations[item[key]] !== undefined ? translations[item[key]] : item[key];
+        } else {
+          decodedItem[key] = item[key];
+        }
+      }
+  
+      return decodedItem;
+    });
 
+    uniqueIds = [...uniqueIds]; // преобразуем в список
+  
+    return {
+      decodedList, // Расшифрованный список
+      uniqueIds, // Список уникальных id
+    };
+  }
 
-
-// console.log(decoded)
+  console.log(decodeFields(encoded, translations));
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,53 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  let amount1 = 1;
+  let amount2 = 0;
+  let rate = 1;
+  let selectedCurrency1 = 'USD';
+  let selectedCurrency2 = 'EUR';
+  let options = ['USD', 'EUR', 'GBP', 'RUB'];
+
+  async function loadRate() {
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${selectedCurrency1}`);
+    const data = await response.json();
+    rate = data.rates[selectedCurrency2];
+    convert1to2();
+  }
+
+  function convert1to2() {
+    amount2 = (amount1 * rate).toFixed(2);
+  }
+
+  function convert2to1() {
+    amount1 = (amount2 / rate).toFixed(2);
+  }
+
+  $: if (selectedCurrency1 && selectedCurrency2) loadRate();
+
+  $: {
+    if (amount1) {
+      convert1to2();
+    }
+  }
+
+  $: {
+    if (amount2) {
+      convert2to1();
+    }
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+ <select bind:value="{selectedCurrency1}">
+          {#each options as option}
+          <option>{option}</option>
+          {/each}
+</select>
+<input type="number" bind:value="{amount1}" on:input="{convert1to2}" />
 
-  <div class="card">
-    <Counter />
-  </div>
+<select bind:value="{selectedCurrency2}">
+  {#each options as option}
+  <option>{option}</option>
+  {/each}
+</select>
+<input type="number" bind:value="{amount2}" on:input="{convert2to1}" />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
